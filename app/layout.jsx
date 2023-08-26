bd9b8d7ee2eb043f8ea4821440a5b2be491b9149
IvanBurenin <140816814+IvanBurenin@users.noreply.github.com>
2023-08-27 00:28:02 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,8 @@
-import localFont from 'next/font/local'
-
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
 import './normalize.css'
 import mainStyles from './main.module.css'
-
-const myFont = localFont({
-  src: [
-    {
-      path: './fonts/RS-semibold.otf',
-    },
-    {
-      path: './fonts/BAHNSCHRIFT.ttf',
-    },
-  ],
-})
 
 export const metadata = {
     title: 'FUSION',
@@ -24,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({children}){
     return (
-      <html style={{height: '100%'}} className={myFont.className}>
+      <html style={{height: '100%'}}>
         <body style={{minHeight: '100%', display: 'flex', flexDirection: 'column'}}>
           <div className={mainStyles.wrapper}>
               <Header/>
